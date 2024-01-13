49af1f4a05bb80638a79c66c8249e8bc7c0e893a
Tetiana-Far <tetiana.far@gmail.com>
2024-01-13 20:38:18 +0200

    Added more weather info

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ function showWeather (response) {
     let temperatureElement = document.querySelector("#current-temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
+    
     temperatureElement.innerHTML = Math.round(temperature);
     cityElement.innerHTML = response.data.city;
 }
