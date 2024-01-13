944613127038d3399e5ccf1cc2097cf9b162f4bb
Tetiana-Far <tetiana.far@gmail.com>
2024-01-13 20:52:11 +0200

    Added time

--- src/index.js
+++ src/index.js
@@ -6,12 +6,33 @@ function showWeather (response) {
     let humidityElement = document.querySelector("#humidity");
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000)
+    temperatureElement.innerHTML = Math.round(temperature);
+    cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
-    
-    temperatureElement.innerHTML = Math.round(temperature);
-    cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+}
+function formatDate(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"];
+    let day = days[date.getDay()];
+    if (hours < 10) {
+        hours = `0${date.getHours()}`
+    };
+     if (minutes < 10) {
+        minutes = `0${date.getMinutes()}`
+    };
+    return `${day} ${hours}:${minutes}`;
 }
 function searchCity(city) {
     let apiKey = "94dfd32atb684f11d63dcb32odcaff90";
