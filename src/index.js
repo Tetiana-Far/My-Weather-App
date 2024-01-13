b3d48c4a131030bc9480298846d0d31db97a4a88
Tetiana-Far <tetiana.far@gmail.com>
2024-01-13 21:18:22 +0200

    Added weather icon

--- src/index.js
+++ src/index.js
@@ -6,13 +6,15 @@ function showWeather (response) {
     let humidityElement = document.querySelector("#humidity");
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
-    let date = new Date(response.data.time * 1000)
+    let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#weather-icon");
     temperatureElement.innerHTML = Math.round(temperature);
     cityElement.innerHTML = response.data.city;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
     timeElement.innerHTML = formatDate(date);
+    iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}" class="weather-icon"></img>`;
 }
 function formatDate(date) {
     let hours = date.getHours();
@@ -27,10 +29,10 @@ function formatDate(date) {
     "Saturday"];
     let day = days[date.getDay()];
     if (hours < 10) {
-        hours = `0${date.getHours()}`
+        hours = `0${hours}`
     };
      if (minutes < 10) {
-        minutes = `0${date.getMinutes()}`
+        minutes = `0${minutes}`
     };
     return `${day} ${hours}:${minutes}`;
 }
